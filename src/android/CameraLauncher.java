52f048540f2f5ecb721cc08c60ce8a5b6a51ec7c
frankrod <frank@acklenavenue.com>
2017-01-05 14:50:12 -0600

    changing android intent to action pick
    commented intent category

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -383,8 +383,8 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 croppedUri = Uri.fromFile(photo);
                 intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, croppedUri);
             } else {
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setAction(Intent.ACTION_PICK);
+//                 intent.addCategory(Intent.CATEGORY_OPENABLE);
             }
         } else if (this.mediaType == VIDEO) {
             intent.setType("video/*");
